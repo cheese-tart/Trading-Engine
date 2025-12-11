5597231d54cca038ceb370fcc83f0b426a681ea9
Chester <w.chester@outlook.com>
2025-12-11 01:05:46 -0800

    Implemented order class

---
+++ backend/engine/README.md
@@ -0,0 +1,1 @@
+AI is a cyclical money machine
--- backend/engine/Server.cpp
+++ backend/engine/Server.cpp
@@ -23,12 +23,12 @@
 enum class OrderType {
     GoodTillCancel,
     FillAndKill
-}
+};
 
 enum class Side {
     Buy,
     Sell
-}
+};
 
 using Price = std::int32_t;
 using Quantity = std::int32_t;
@@ -37,18 +37,61 @@ using OrderId = std::int64_t;
 struct LevelInfo {
     Price price_;
     Quantity quantity_;
-}
+};
 
-using LevelInfos_t = std::vector<LevelInfo>;
+using LevelInfos = std::vector<LevelInfo>;
 
 class OrderbookLevelInfos {
     public:
-        OrderbookLevelInfos(LevelInfos& bids, LevelInfos& asks)
+        OrderbookLevelInfos(const LevelInfos& bids, const LevelInfos& asks)
             : bids_{ bids }
             , asks_{ asks }
-        {}
-}
+        { }
+
+        const LevelInfos& GetBids() const { return bids_; }
+        const LevelInfos& GetAsks() const { return asks_; }
+
+    private:
+        LevelInfos bids_;
+        LevelInfos asks_;
+};
+
+class Order {
+    public:
+        Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
+            : orderType_{ orderType }
+            , orderId_{ orderId }
+            , side_{ side }
+            , price_{ price }
+            , initialQuantity_{ quantity }
+            , remainingQuantity_{ quantity }
+        { }
+
+        OrderId GetOrderId() const { return orderId_; }
+        Side GetSide() const { return side_; }
+        Price GetPrice() const { return price_; }
+        OrderType GetOrderType() const { return orderType_; }
+        Quantity GetInitialQuantity() const { return initialQuantity_; }
+        Quantity GetRemainingQuantity() const { return remainingQuantity_; }
+        Quantity GetFilledQuantity() const { return GetInitialQuantity() - GetRemainingQuantity(); }
+        void Fill(Quantity quantity) {
+            if (quantity > GetRemainingQuantity()) {
+                throw std::logic_error(std::format("Order({}) cannot be filled for more thatn its remaining quantity.", GetOrderId()));
+            }
+            remainingQuantity_ -= quantity;
+        }
+    
+    private:
+        OrderType orderType_;
+        OrderId orderId_;
+        Side side_;
+        Price price_;
+        Quantity initialQuantity_;
+        Quantity remainingQuantity_;
+};
+
+using OrderPointer = std::make_shared<Order>;
 
 int main() {
     return 0;
-}
+};
